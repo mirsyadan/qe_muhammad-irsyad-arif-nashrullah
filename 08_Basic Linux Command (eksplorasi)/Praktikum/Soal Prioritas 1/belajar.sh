bbaa16a1fda6d5a56b035d652eaca54b68619861
mirsyadan <mohirsyad09@gmail.com>
2024-04-23 21:22:34 +0700

    ADD FILE .SH

---
+++ 08_Basic Linux Command (eksplorasi)/Praktikum/Soal Prioritas 1/belajar.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Menampilkan tanggal dan waktu saat ini
+echo "Tanggal dan Waktu Sekarang:"
+date
+
+# Menampilkan informasi sistem
+echo "Informasi Sistem:"
+uname -a
+
+# Menampilkan pengguna yang saat ini masuk
+echo "Pengguna yang Masuk:"
+whoami
+
+# Menampilkan daftar file dalam direktori saat ini
+echo "Daftar File dalam Direktori Saat Ini:"
+ls
+
+# Menampilkan ruang disk yang digunakan dan tersisa
+echo "Ruang Disk yang Digunakan dan Tersisa:"
+df -h
+
